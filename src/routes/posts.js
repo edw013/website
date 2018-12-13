fd58c1b637da6137ab7fb5ed2b97ba23a1269064
Ethan <ehwang@salesforce.com>
2018-12-13 15:09:25 -0800

    List and create comments

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -18,26 +18,13 @@ const ObjectId = require("mongodb").ObjectID;
  */
 router.get("/", async (req, res) => {
     const find = {};
-    const project = {
-        _id: 1,
-        title: 1,
-        body: 1,
-        date: 1,
-        numComments: {
-            $size: "$comments"
-        }
-    };
     const sort = {
         date: -1
     };
 
     const db = req.app.locals.db;
     const postCollection = db.collection("posts");
-    const posts = await postCollection.aggregate([
-        { $match: find },
-        { $project: project},
-        { $sort: sort }
-    ]).toArray();
+    const posts = await postCollection.find(find).sort(sort).toArray();
 
     res.status(200).send({ posts: posts });
 });
@@ -49,21 +36,17 @@ router.get("/", async (req, res) => {
  *   id: the object ID within Mongo
  * 
  * Response codes:
+ *   200: success
  *   400: invalid id format
  *   404: not found
- *   200: success
  */
 router.get("/:id", async (req, res) => {
     const postId = validator.escape(req.params["id"]);
 
-    let oid;
-    try {
-        oid = new ObjectId(postId);
-    }
-    catch (err) {
-        console.error(err);
+    const oid = createObjectId(postId);
 
-        return res.sendStatus(400)
+    if (oid === null) {
+        return res.sendStatus(400);
     }
 
     const find = {
@@ -74,12 +57,121 @@ router.get("/:id", async (req, res) => {
     const postCollection = db.collection("posts");
     const post = await postCollection.findOne(find);
 
+    // does it exist?
     if (post === null) {
         return res.sendStatus(404);
     }
 
     res.status(200).send(post);
-})
+});
+
+/**
+ * Get comments on a specific post.
+ * 
+ * Request parameters:
+ *   id: the object ID within Mongo
+ * 
+ * Response codes:
+ *   200: success 
+ */
+router.get("/:id/comments", async (req, res) => {
+    const postId = validator.escape(req.params["id"]);
+
+    // check valid oid, we don't really care if it doesn't exist as we'll
+    // just return [] if it doesn't
+    const oid = createObjectId(postId);
+    if (oid === null) {
+        return res.sendStatus(400);
+    }
+
+    const find = {
+        pid: oid
+    };
+    const sort = {
+        date: -1
+    };
+
+    const db = req.app.locals.db;
+    const commentCollection = db.collection("comments");
+    const comments = await commentCollection.find(find).sort(sort).toArray();
+    
+    res.status(200).send({ comments: comments });
+});
+
+/**
+ * Create a new comment on a post.
+ * 
+ * Request parameters:
+ *   id: the object ID of the post
+ * 
+ * Request body JSON:
+ *   body: the body of the comment
+ * 
+ * Response codes:
+ *   201: created
+ *   400: missing body
+ *   404: post does not exist
+ *   500: error inserting into DB
+ */
+router.post("/:id/comments/new", jsonParser, async (req, res) => {
+    const postId = validator.escape(req.params["id"]);
+
+    // check valid oid
+    const oid = createObjectId(postId);
+    if (oid === null) {
+        return res.sendStatus(400);
+    }
+
+    const findPost = {
+        _id: oid
+    };
+
+    const db = req.app.locals.db;
+    const postCollection = db.collection("posts");
+
+    // check if post exists first
+    const post = await postCollection.findOne(findPost);
+
+    if (post === null) {
+        return res.sendStatus(404);
+    }
+
+    // get comment body
+    const body = validator.escape(req.body["body"]);
+    
+    if (!body) {
+        res.sendStatus(400);
+
+        return;
+    }
+
+    const date = new Date();
+
+    const data = {
+        pid: oid,
+        date: date,
+        lastUpdate: date,
+        body: body
+    };
+
+    const commentCollection = db.collection("comments");
+    commentCollection.insertOne(data, async err => {
+        if (err) {
+            console.error(err);
+
+            return res.sendStatus(500);
+        }
+
+        const inc = {
+            $inc: { numComments: 1 }
+        };
+    
+        // increment numComments
+        await postCollection.updateOne(findPost, inc);
+
+        res.sendStatus(201);
+    });
+});
 
 /**
  * Create a new post.
@@ -89,9 +181,9 @@ router.get("/:id", async (req, res) => {
  *   body: the body of the post
  * 
  * Response codes:
+ *   201: created
  *   400: missing title or body
  *   500: error inserting into DB
- *   201: created
  */
 router.post("/new", jsonParser, async (req, res) => {
     const title = validator.escape(req.body["title"]);
@@ -109,7 +201,8 @@ router.post("/new", jsonParser, async (req, res) => {
         title: title,
         body: body,
         date: date,
-        comments: []
+        lastUpdate: date,
+        numComments: 0
     }
     
     const db = req.app.locals.db;
@@ -125,5 +218,19 @@ router.post("/new", jsonParser, async (req, res) => {
         res.sendStatus(201);
     });
 });
+
+const createObjectId = id => {
+    let oid;
+    try {
+        oid = new ObjectId(id);
+    }
+    catch (err) {
+        console.error(err);
+
+        oid = null;
+    }
+
+    return oid;
+}
 
 module.exports = router;
